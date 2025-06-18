ec46c15f39049117b4453094790277a9b3b900d9
banbezv <banbezv@mail.ru>
2025-06-18 08:18:54 +0300

    HW-Spring-Scopes

--- src/main/java/org/skypro/skyshop/service/SearchService.java
+++ src/main/java/org/skypro/skyshop/service/SearchService.java
@@ -4,10 +4,7 @@ import org.skypro.skyshop.model.search.SearchResult;
 import org.skypro.skyshop.model.search.Searchable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 @Service
@@ -19,16 +16,11 @@ public class SearchService {
     }
 
     public Collection<SearchResult> search(String pattern) {
-        Function<Searchable, String> func = Searchable::getSearchTerm;
-        Function<String, Boolean> func2 = (str) -> str.contains(pattern);
-        Function<Searchable, Boolean> composition = func2.compose(func);
-        Predicate<Searchable> predicate = (searchable) -> composition.apply(searchable);
         Collection<Searchable> allSearchables = storageService.getAllSearchables();
         Stream<Searchable> stream = allSearchables.stream();
-        Collection<SearchResult> results = stream.filter(predicate)
-                .map(searchable -> SearchResult.fromSearchable(searchable))
+        return stream.filter(searchable->searchable.getSearchTerm().contains(pattern))
+                .map(SearchResult::fromSearchable)
                 .toList();
-        return results;
     }
 
     public StorageService getStorageService() {
